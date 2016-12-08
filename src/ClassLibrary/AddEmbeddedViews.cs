039ba2e748c6c7ae6f9149097c724b182a698336
arichika taniguchi <email@arichika.net>
2016-12-08 11:52:10 +0900

    init.

---
+++ src/ClassLibrary/AddEmbeddedViews.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.FileProviders;
+using System.Reflection;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ClassLibraryEmbeddedViewsExtensions
+    {
+        public static IServiceCollection AddEmbeddedViews(this IServiceCollection services)
+        {
+            var optionsSetup = new ConfigureOptions<RazorViewEngineOptions>(options =>
+            {
+                options.FileProviders.Add(
+                    new EmbeddedFileProvider(
+                        typeof(ClassLibraryEmbeddedViewsExtensions).GetTypeInfo().Assembly,
+                        "ClassLibrary"));
+            });
+            services.AddSingleton<IConfigureOptions<RazorViewEngineOptions>>(optionsSetup);
+
+            return services;
+        }
+    }
+}
